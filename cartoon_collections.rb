be897a46da14dee8666ae7a54a23d5e5400f06e5
michael onyeije <monyeije16@saintpeters.edu>
2020-09-29 21:46:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,6 +32,8 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.each{ |i|
-  return valid_calls.include? i
+  if valid_calls.include? i
+    return i
+  else return nil
 }
 end
