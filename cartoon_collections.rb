80e3cd41517497a9722ad152b334ecb76ca51303
michael onyeije <monyeije16@saintpeters.edu>
2020-09-29 22:08:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -35,10 +35,9 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.each{ |i|
   if valid_calls.include? i
-    puts i 
-    return i
+    print i
   else
-    return nil
+    print nil
   end
 }
 end
